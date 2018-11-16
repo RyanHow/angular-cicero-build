540f1dfb8634026effef45bef6a923a3304c751b
Ryan How <ryan@zbit.net.au>
2018-11-16 12:56:01 +0800

    add minimal cicero libraries, import and custom webpack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Template, Clause } from '@accordproject/cicero-core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cicero-build';
+  template: any;
+  clause: any;
+
+  constructor() {
+
+      const packageJson = {
+      'name': 'test-contract',
+      'version': '0.1.0',
+      'description': 'test',
+      'cicero': {
+        'template': 'contract',
+        'language': 'ergo',
+        'version': '^0.9.1'
+      }
+    };
+
+    this.template = new Template(packageJson, undefined, {txt: 'Sample text'});
+    this.clause = new Clause(this.template);
+
+  }
 }
