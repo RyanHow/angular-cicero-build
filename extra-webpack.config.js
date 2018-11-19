81da66aa0bf42ff9d47353451b1c0e27f99120ad
Ryan How <ryan@zbit.net.au>
2018-11-19 15:06:04 +0800

    fix(smart-contracts): cicero terser workaround

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,4 +1,32 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
+    optimization: {
+        minimizer: [new TerserPlugin({
+            parallel: true,
+            cache: true,
+            terserOptions: {
+                ecma: undefined,
+                warnings: false,
+                parse: {},
+                compress: {
+                    warnings: false, // Suppress uglification warnings
+                    pure_getters: true,
+                    unsafe: true,
+                    unsafe_comps: true
+                },
+                mangle: true, // Note `mangle.properties` is `false` by default.
+                module: false,
+                output: null,
+                toplevel: false,
+                nameCache: null,
+                ie8: false,
+                keep_classnames: undefined,
+                keep_fnames: false,
+                safari10: false
+            }
+        })]
+    },
     node: {
         fs: 'empty',
         net: 'empty',
